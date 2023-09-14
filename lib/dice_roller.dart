f3e03b49319c27f8bd558db7f236e3e10779d049
Isharab Ahmed <isharab.dev@gmail.com>
2023-09-14 23:56:19 +0500

    done with the roll dice app

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -9,10 +12,12 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-2.png';
+  var currentDiceRoll = 1;
 
   void rollDice() {
-    activeDiceImage = 'assets/images/dice-1.png';
+    setState(() {
+      currentDiceRoll = randomizer.nextInt(6) + 1;
+    });
   }
 
   @override
@@ -21,7 +26,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/images/dice-$currentDiceRoll.png',
           width: 250,
         ),
         const SizedBox(
